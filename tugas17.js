f1048c05345445f3853a17f4f7cf068a838a359d
Martino Wagey <martinowagey@gmail.com>
2019-07-22 16:55:34 +0700

    add tugas 17 JS

---
+++ tugas17.js
@@ -0,0 +1,19 @@
+function upperLower() {
+  var a = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+  var b = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+
+  console.log(a.toLowerCase());
+  console.log(a.toUpperCase());
+  console.log(b.toLowerCase());
+  console.log(b.toUpperCase());
+}
+
+upperLower();
+
+function ascii() {
+  var i = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+  console.log(i.charCodeAt(0),i.charCodeAt(1),i.charCodeAt(2),i.charCodeAt(3),i.charCodeAt(4),i.charCodeAt(5));
+}
+
+ascii();
